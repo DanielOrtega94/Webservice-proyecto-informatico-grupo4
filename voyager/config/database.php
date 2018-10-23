f6488344b81e4f6a30797c19f11d3b3a241eb218
Daniel Ortega Carcamo <18015929+DanielOrtega94@users.noreply.github.com>
2018-10-23 16:36:52 -0300

    base

--- voyager/config/database.php
+++ voyager/config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+             'host' => env('DB_HOST', 'bdd.inf.udec.cl'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'proyinf4'),
+            'username' => env('DB_USERNAME', 'proyinf4'),
+            'password' => env('DB_PASSWORD', 'chiloteql0'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
